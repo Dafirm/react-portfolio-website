fa914ad3095948dd04713c517383fa2e2b31dab9
Femi Oyeniyi <65442229+Dafirm@users.noreply.github.com>
2022-07-06 18:22:25 +0300

    setting up components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
-import Testimonials from './components/testimonials/Testimonials'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
@@ -19,7 +18,6 @@ const App = () => {
         <Experience />
         < Services/>
         <Portfolio />
-        <Testimonials />
         <Contact />
         <Footer />
     </>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css';
+import { FaFacebook } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
+import {IoLogoTwitter} from 'react-icons/io';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Dafirm</a>
+
+      <ul className='permalinks'>
+        <li><a href= '#'>Home</a></li>
+        <li><a href = '#about'>About</a></li>
+        <li><a href = '#experience'>Experience</a></li>
+        <li><a href = '#services'>Services</a></li>
+        <li><a href = '#portfolio'>Portfolio</a></li>
+        <li><a href = '#contact'>contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href= "http://facebook.com"><FiInstagram/></a>
+        <a href= "http://facebook.com"><IoLogoTwitter/></a>
+        <a href= "http://facebook.com">< FaFacebook/></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy;All right reserved. Developed by Dafirm  </small>
+      </div>
+    </footer>
   )
 }
 
